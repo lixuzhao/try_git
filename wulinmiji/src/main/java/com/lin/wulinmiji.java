554669157b1a2df1ac78f2965e6e6eb4cb7035c0
lixuzhao <1071246892@qq.com>
2018-12-06 14:05:17 +0800

    武林秘籍

---
+++ wulinmiji/src/main/java/com/lin/wulinmiji.java
@@ -0,0 +1,18 @@
+package com.lin;
+
+/**
+ * Created by LZX on 2018/12/6.
+ */
+public class wulinmiji {
+    public static void main(String[] args) {
+        System.out.println("hello meinv");
+        System.out.println("hello meinv");
+        System.out.println("hello meinv");
+        System.out.println("hello meinv");
+        System.out.println("hello meinv");
+
+    }
+
+
+
+}
